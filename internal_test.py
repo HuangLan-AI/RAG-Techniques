e49f6d3719d2014a1746a697d33d10a92a1dcee8
HuangLan-AI <huanglan.sarah@gmail.com>
2024-11-28 13:57:58 +0800

    Add Expansion Queries

--- internal_test.py
+++ internal_test.py
@@ -90,13 +90,17 @@ retrieved_documents_naive = results["documents"][0]
 #     print("\n")
 
 
+"""
+Expansion Answer
+"""
 ###########################################
 # Generate Hallucinated Answers
 ###########################################
 # Create a answer generating model using base model gpt-3.5-turbo
 def augment_query_generated(query, model="gpt-3.5-turbo"):
     prompt = """You are an experienced and helpful academic researcher. 
-   Provide an example answer to the given question, that might be found in a document of story."""
+                Provide an example answer to the given question based on common sense in life, 
+                that might be found in a document of story. Keep your answer into one paragraph."""
     messages = [
         {
             "role": "system",
@@ -116,22 +120,71 @@ def augment_query_generated(query, model="gpt-3.5-turbo"):
 original_query = "How the goldfish die?"
 hypothetical_answer = augment_query_generated(original_query)
 joint_query = f"{original_query} {hypothetical_answer}"
-# print(word_wrap(joint_query))
+print(f"For Expansion Answer, the joint query is {joint_query}")
 
-
-###########################################
-# Get answers from chunks
-###########################################
 # Pass both original query and hallucinated answer to chromabd
 results = chroma_collection.query(
     query_texts=joint_query, n_results=5, include=["documents", "embeddings"]
 )
 retrieved_documents_exp_ans = results["documents"][0]
 
-# for doc in retrieved_documents:
-#     print(word_wrap(doc))
-#     print("")
 
+"""
+Expansion Queries
+"""
+# Create a queries generating model
+def generate_multi_query(query, model="gpt-3.5-turbo"):
+
+    prompt = """
+    You are a knowledgeable academic researcher. 
+    Your users are inquiring about a story. 
+    For the given question, propose up to five related questions to assist them in finding the information they need. 
+    Provide concise, single-topic questions (withouth compounding sentences) that cover various aspects of the topic. 
+    Ensure each question is complete and directly related to the original inquiry. 
+    List each question on a separate line without numbering.
+                """
+
+    messages = [
+        {
+            "role": "system",
+            "content": prompt,
+        },
+        {"role": "user", "content": query},
+    ]
+
+    response = client.chat.completions.create(
+        model=model,
+        messages=messages,
+    )
+    content = response.choices[0].message.content
+    content = content.split("\n")
+    return content
+
+original_query = (
+    "How the goldfish die?"
+)
+aug_queries = generate_multi_query(original_query)
+
+# Concatenate the original query with the augmented queries
+joint_query = [
+    original_query
+] + aug_queries
+
+results = chroma_collection.query(
+    query_texts=joint_query, n_results=5, include=["documents", "embeddings"]
+)
+retrieved_documents_exp_que = results["documents"]
+
+# Deduplicate the retrieved documents
+unique_documents = set()
+for documents in retrieved_documents_exp_que:
+    for document in documents:
+        unique_documents.add(document)
+retrieved_documents_exp_que = unique_documents
+
+###########################################
+# Get answers from chunks
+###########################################
 # Generate the final answer using the OpenAI model
 def generate_answer(query, context, model="gpt-3.5-turbo"):
 
@@ -161,10 +214,17 @@ def generate_answer(query, context, model="gpt-3.5-turbo"):
 
 context_naive = "\n\n".join(retrieved_documents_naive)
 context_exp_ans = "\n\n".join(retrieved_documents_exp_ans)
+context_exp_que = "\n\n".join(retrieved_documents_exp_que)
 
 res_naive = generate_answer(query=original_query, context=context_naive)
 res_exp_answer = generate_answer(query=original_query, context=context_exp_ans)
+res_exp_que = generate_answer(query=original_query, context=context_exp_que)
+
 print("Final Answer of Navive RAG:")
 print(res_naive)
+print("=================================")
 print("Final Answer of Expansion Answer:")
 print(res_exp_answer)
+print("=================================")
+print("Final Answer of Expansion Queries:")
+print(res_exp_que)
